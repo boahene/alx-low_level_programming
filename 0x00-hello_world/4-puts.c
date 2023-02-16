d0f9d0448b0473f8d05e755fff889634b7115b22
boahene <oseiwusuboahene@gmail.com>
2023-02-16 12:54:40 -0800

    C program that prints output,followed by a new line use puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program thats prints using puts function instead of printf
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+
+	puts("\"Programing is like building a multilingual puzzle");
+	return (0);
+}
