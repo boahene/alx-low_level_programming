6a87717050ef9713b7676c230a58e0d51ed77575
boahene <oseiwusuboahene@gmail.com>
2023-02-16 17:37:15 -0800

    print an output followed by a new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that prints a line to the standard error
  *
- * Return: 1 (success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
