23a9236e42f3f28b3194d4315499acdd8deb7fcc
boahene <oseiwusuboahene@gmail.com>
2023-02-18 07:07:36 -0800

    program to print data on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %zu bytes(s)\n", sizeof(a));
-	printf("Sizze of a int: %zu bytes(s)\n", sizeof(b));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(b));
 	printf("Size of a long int: %zu bytes(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu bytes(s)\n", sizeof(d));
 	printf("Size of a float: %zu bytes(s)\n", sizeof(f));
