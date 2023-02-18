203c0b748bdf50589bb2388a4ea4b3112c1f16f1
boahene <oseiwusuboahene@gmail.com>
2023-02-17 19:57:41 -0800

    checker updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	}
 	else if ((n % 10)  == 0)
 	{
-		printf("last digit of %d is %d and if equal to 0\n", n, n % 10);
+		printf("last digit of %d is %d and if is 0\n", n, n % 10);
 	}
 	else if ((n % 10)  < 6)
 	{
-		printf("last digit of %d is  %d and is less than 6  not 0\n", n, n % 10);
+		printf("last digit of %d is  %d and is less than 6 and  not 0\n", n, n % 10);
 	}
 	return (0);
 }
