8044a46928ae40132cb002eb1f4fc59990d76126
boahene <oseiwusuboahene@gmail.com>
2023-02-18 04:26:37 -0800

    a program that rpints numbers, with space and comma

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Program that prints hexadecimal
+ * there should be a next line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0');
+	}
+	for (ch = 'a'; che <= 'f'; ch++);
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
