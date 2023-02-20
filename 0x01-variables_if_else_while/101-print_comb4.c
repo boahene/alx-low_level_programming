8991e76325c71044f4a56c1cf2b01e0ef9ab9366
boahene <oseiwusuboahene@gmail.com>
2023-02-20 12:47:50 -0800

    updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - A program that prints three digit followed by 
+ * main - A program that prints three digit followed by
  * a comma and space
  * Return: Always 0 (Success)
  */
@@ -10,23 +10,23 @@ int main(void)
 {
 	int d, p, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (p = '0'; p <= '9'; p++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (q = '0'; q <= '9'; q++)
 			{
-				if ((p != d) != q)
+				if (d < p && p < q)
 				{
 					putchar(d);
 					putchar(p);
 					putchar(q);
 
-					if (d == '7' && p == '8')
-						continue;
-
-					putchar(',');
-					putchar(' ');
+					if (d != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
