6284d045e863062846be11dcbdc2679441c17782
boahene <oseiwusuboahene@gmail.com>
2023-02-23 17:00:45 -0800

    diagonal display

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal line made of backlashes
+ * @n: number of \  to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+				_putchar(' ');
+			_putchar('\\');
+			if (x == (n - 1))
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+
+}
