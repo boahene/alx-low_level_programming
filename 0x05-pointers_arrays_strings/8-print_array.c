6485729fe4b9df51ac4cb6f979e8c644e9a516eb
boahene <oseiwusuboahene@gmail.com>
2023-02-28 03:58:13 -0800

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	for (n--; n >= 0; n--, i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if (n > 0)
-		{
-			print(*, *);
-		}
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
-	printf("\n";
+	printf("\n");
 }
