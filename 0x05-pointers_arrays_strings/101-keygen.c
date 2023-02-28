b799a48a9d5855f1815d127222f25bf20d9654a6
boahene <oseiwusuboahene@gmail.com>
2023-02-28 05:34:56 -0800

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,32 +3,13 @@
 #include <string.h>
 
 /**
- * main -  generate code to crackme
+ * main -  generates code to carck
  *
- * Return: Always 0 (Success)
+ * Return: Always 0. (Success)
  */
+
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
-
-	sum = 0;
-
-	srand(tiime(NULL));
-
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	}
-
+	printf("Tada! Congrats");
 	return (0);
 }
