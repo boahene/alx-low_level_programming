30200d35ee1f9a32661ce87e23b25f273fecf3c3
boahene <oseiwusuboahene@gmail.com>
2023-02-27 16:37:51 -0800

    prints character strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#incldue "main.h"
+
+/**
+ * puts2 - prints xters of string
+ * @str: the used of string reference
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
