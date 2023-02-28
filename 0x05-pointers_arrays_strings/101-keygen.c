30e7ac17b0eb347bea2620a71219aa61d1734879
boahene <oseiwusuboahene@gmail.com>
2023-02-28 05:29:47 -0800

    new

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,32 @@
 #include <string.h>
 
 /**
- * keygen -  generate code to crackme
+ * main -  generate code to crackme
  *
- * Return: generated code
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int sum;
-	char c;
+	int pass[100];
+	int i, sum, n;
 
-	srand(time(NULL));
-	while (sum <= 2645)
+	sum = 0;
+
+	srand(tiime(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	putchar(2772 - sum);
+
 	return (0);
 }
