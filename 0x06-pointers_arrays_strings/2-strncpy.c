7d357c2a97a553a65521649204de8243e94bf8de
boahene <oseiwusuboahene@gmail.com>
2023-03-02 15:04:02 -0800

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: delete your content
  * @src: copy your files into memory
  * @n: shorten the src value
+ *
  * Return: return the value in the memory into dest
  */
 
@@ -18,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 
-	for (index = sr _len; index < n; index++)
+	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
