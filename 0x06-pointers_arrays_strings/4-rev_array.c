59a98dae59711b5ca957c00eef6fb46bc2074095
boahene <oseiwusuboahene@gmail.com>
2023-03-02 15:36:11 -0800

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+
+/**
+ * reverse_array - rverse the order of nth array
+ * @a: reverse the array
+ * @n: this should be the length of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, count;
+
+	n = n - 1;
+	count = 0;
+	while (count <= n)
+	{
+		i = s[count];
+		a[count++] = a[n];
+		a[n--] = i;
+	}
+}
