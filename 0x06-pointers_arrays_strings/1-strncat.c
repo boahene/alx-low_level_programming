80d99e447e67db998c58305d1829deec7dcf0bcb
boahene <oseiwusuboahene@gmail.com>
2023-03-02 14:53:00 -0800

    homeboy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int c, i;
+	int index = 0, dest_len = 0;
 
-	c = 0;
-	while (dest[c])
-		c++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i} = src[i];
-		dest[c + i] = '\0';
-
-		return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
