a3ea358927b1cc192bf95d5ccfd7eda96bede91a
boahene <oseiwusuboahene@gmail.com>
2023-03-02 14:18:38 -0800

    new update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,19 @@
 
 int_strcmp(char *s1, char *s2)
 {
-	while ((*s1 && *s2) && (s1 == s2))
+	int i, flag = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			flag = s1[i] - s2[i];
+		}
+		else
+		{
+			continue;
+		}
+		return (flag);
 	}
-	return (*s1 - *s2);
+	return (0);
 }
