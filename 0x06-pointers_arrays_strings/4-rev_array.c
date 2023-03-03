7fef151752e40103f8af1ea0b1345604a73faebb
boahene <oseiwusuboahene@gmail.com>
2023-03-02 17:04:07 -0800

    new rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temp, index;
+	int i, count;
 
-	for (index = n - 1; index >= n / 2; index--)
+	n = n - 1;
+	count = 0;
+	while (count <= n)
 	{
-		temp =  a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = temp;
+		i = a[count];
+		a[count++] = a[n];
+		a[n--]  = i;
 	}
 }
