d3e776686ae0ee9d8fdb3edd83ea6f51b93aefd2
boahene <oseiwusuboahene@gmail.com>
2023-03-02 16:25:50 -0800

    reviesed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 	int temp, index;
 
-	for (index = n - 1 - index >= n / 2; index--)
+	for (index = n - 1; index >= n / 2; index--)
 	{
 		temp =  a[n - 1 - index];
-		an[n - 1 - index] = a[index];
+		a[n - 1 - index] = a[index];
 		a[index] = temp;
 	}
 }
