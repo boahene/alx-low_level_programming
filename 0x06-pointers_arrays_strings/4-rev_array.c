743cd20cb39d429c7b017edb3fe716dfcb258222
boahene <oseiwusuboahene@gmail.com>
2023-03-02 16:20:24 -0800

    reverses the content

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,19 +4,15 @@
  * reverse_array - rverse the order of nth array
  * @a: reverse the array
  * @n: this should be the length of the array
- * Return: void
  */
-
 void reverse_array(int *a, int n)
 {
-	int i, count;
+	int temp, index;
 
-	n = n - 1;
-	count = 0;
-	while (count <= n)
+	for (index = n - 1 - index >= n / 2; index--)
 	{
-		i = a[count];
-		a[count++] = a[n];
-		a[n--] = 1;
+		temp =  a[n - 1 - index];
+		an[n - 1 - index] = a[index];
+		a[index] = temp;
 	}
 }
