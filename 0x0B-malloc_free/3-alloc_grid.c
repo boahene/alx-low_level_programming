86f13781111d737a879cfebe610a30698918d397
boahene <oseiwusuboahene@gmail.com>
2023-03-14 15:45:14 -0700

    mallock

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - entry point
+ * @width: get the width for the matrcs
+ * @height: get the height of matrics
+ * Return: return the result
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int j, i;
+	int **arr;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	err = (int **)malloc(sizeof(int *) * height);
+	if (err == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+
+	for (i =0; i < height; i++)
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	return (arr):
+}
