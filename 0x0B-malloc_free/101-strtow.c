f182425b337d400547169cba8ae3476694101262
boahene <oseiwusuboahene@gmail.com>
2023-03-14 18:46:06 -0700

    corrected

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,99 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int word_len(char *str);
+int count_word(char *str);
+char **strtow(char *str);
+
+/**
+ * world_len - Locate the index marking the
+ *             first word contained within a string
+ * @str: the string to be searched.
+ *
+ * Return: the index marking the end of the initial word pointed to by str.
+ */
+int world_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+
+/**
+ * count_words - counts the number of words conatined within a string
+ * @str: The string to be searched.
+ *
+ * Return: The number of ward contained within str.
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+
+	return (words);
+}
+
+/**
+ * strtot - Splits a string into words
+ * @str: the string to be spirit.
+ *
+ * Return: if str = NULL, str = "", or the function
+ */
+char **strtot(char *str)
+{
+	char **strings;
+	int index = 0, words, w letters, 1;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = malloc(sieof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (1 = 0; 1 < letters; 1++)
+			strings[w][1] = str[index++];
+
+		strings[w][i] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
+}
