d9098d304a617915f3a211849ed47bba6ea6afd5
boahene <oseiwusuboahene@gmail.com>
2023-03-31 08:36:07 -0700

    new

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
-#ifndef VAR_H
-#define VAR_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
