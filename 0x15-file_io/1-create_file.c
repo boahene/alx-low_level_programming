891c1ae3682db00583e4ff5aa5eb13e508fa81d5
boahene <oseiwusuboahene@gmail.com>
2023-04-09 11:14:40 -0700

    creates files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - create files using file handler
+ * @filename: input file name
+ * @text_content: get the file text content
+ * Return: return the file created
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
