d1ca89e3c65aa8d64fdb3a742b2ebba7d95e8a1a
boahene <oseiwusuboahene@gmail.com>
2023-04-09 03:10:41 -0700

    convert values to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts  to decimal
+ * @b: get the input from user
+ *
+ * Return: return the converted value to standard output
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, dec = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
+	}
+	return (dec);
+}
