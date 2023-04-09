647d4671479020ea151fe58251090d4c231965ef
boahene <oseiwusuboahene@gmail.com>
2023-04-09 04:02:22 -0700

    value of bit and index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - set bit length value
+ * @index: get the index of bits
+ * @n: bits
+ * Return: return new bits
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+
+	return (1);
+}
