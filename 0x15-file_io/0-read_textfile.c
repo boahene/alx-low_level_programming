20aead1f32d54f79db3f8c62f248998b328bcb41
boahene <oseiwusuboahene@gmail.com>
2023-04-09 11:00:47 -0700

    new content

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - entry whre files are pinted into file
+ * @filename: point to the file in the memory
+ * @letters: print these letters
+ * Return: return the values
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+	w = write(STDOUT_FILENO, text, let);
+	close(file);
+
+	return (w);
+}
